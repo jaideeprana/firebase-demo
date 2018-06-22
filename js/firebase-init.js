5386ff3ddc2b8232a7492755a2fa72ece0978f6b
Jaideep Rana <jaideepk@thoughtworks.com>
2018-06-22 13:22:41 +0530

    000 | [Jaideep] | Init a firebase-web project

---
+++ js/firebase-init.js
@@ -0,0 +1,9 @@
+let config = {
+    apiKey: "AIzaSyDY5pbBSsNi5r24IxoagbP0S_H3hsxLs8w",
+    authDomain: "my-first-firebase-projec-9c265.firebaseapp.com",
+    databaseURL: "https://my-first-firebase-projec-9c265.firebaseio.com",
+    projectId: "my-first-firebase-projec-9c265",
+    storageBucket: "",
+    messagingSenderId: "346352052042"
+};
+firebase.initializeApp(config);
